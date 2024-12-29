db1cff4154f9efecc58fc7150fd001f90bb005f8
w-haibara <hwhaibarawataru@gmail.com>
2024-12-29 22:11:21 +0900

    tmp: impl cgi

---
+++ src/cgi.ts
@@ -0,0 +1,6 @@
+export function Exec(target: string, params: string): string {
+  console.log(target);
+  console.log(params);
+
+  return "";
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { Exec } from "./cgi";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+app.post("/cgi/:target", async (c) => {
+  const target = c.req.param("target");
+  const params = await c.req.text();
+
+  try {
+    const res = Exec(target, params);
+
+    return c.text(res);
+  } catch {
+    throw new HTTPException(500);
+  }
 });
 
 export default app;
